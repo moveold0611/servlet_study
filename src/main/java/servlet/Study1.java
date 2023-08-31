3e0ff7f4459ff59e78956a19f84d595b81fa2540
DESKTOP-NUOPUIF\ITPS <b44sup97045@gmail.com>
2023-08-31 11:59:18 +0900

    feat: test_commit

---
+++ src/main/java/servlet/Study1.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/study/1") // endpoint(요청주소)
+public class Study1 extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("get 요청");
+	}
+
+	
+}
